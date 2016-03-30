a4858c5a561a7aae5bff2be44f38da01a33478f4
TrackerJr <dansonthaya@gmail.com>
2016-03-30 22:12:51 +0700

    All files

---
+++ grails-app/assets/javascripts/landing/admin/landing.admin.js
@@ -0,0 +1,9 @@
+//= wrapped
+//= require /angular/angular
+//= require /landing/admin/core/landing.admin.core
+//= require /landing/admin/index/landing.admin.index
+
+angular.module("landing.admin", [
+    "landing.admin.core",
+    "landing.admin.index"
+]);
